d6883ddfdbaa86ee9d675a772345e9fa916e65e3
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2022-05-29 23:05:41 +0700

    update

---
+++ README.md
@@ -0,0 +1,17 @@
+# 2mint Recipes Import Data
+
+## Installation
+
+```bash
+cd functions
+npm install
+```
+
+## Run
+
+```bash
+cd functions
+npm run serve
+```
+
+Docs available at [http://localhost:5000/docs](http://localhost:5000/docs).
--- functions/index.js
+++ functions/index.js
@@ -9,14 +9,6 @@ app.use(express.json());
 app.use(router);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
   swaggerDocs(app, port);
 });
-
-// app.get('/', (req, res) => res.status(200).send('Hey there!'));
-// app.post('/entries', addEntry);
-// app.get('/entries', getAllEntries);
-// app.delete('/entries/:entryId', deleteEntry);
-// app.patch('/entries/:entryId', updateEntry);
-
-// exports.app = functions.https.onRequest(app);
